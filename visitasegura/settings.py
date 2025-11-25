17417cc7f23ac5bb5ad785216ca782331783031c
OctavioCDS <oc.figueroa@duocuc.cl>
2025-11-25 13:17:01 -0300

    solucion al error 400

--- visitasegura/settings.py
+++ visitasegura/settings.py
@@ -22,15 +22,13 @@ DEBUG = env.bool("DEBUG", default=True)
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    # cuando tengas el dominio/URL de Railway lo agregas aquí, por ejemplo:
-    # "mi-proyecto.up.railway.app",
+    "capstone-project-production-6b24.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    # y aquí la versión https de Railway cuando la tengas, ej:
-    # "https://mi-proyecto.up.railway.app",
+    "https://capstone-project-production-6b24.up.railway.app",
 ]
 
 # =========================
